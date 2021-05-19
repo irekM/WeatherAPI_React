622710d7dce2a6e1fad9a36580e7358dd6932f49
Your Name <you@example.com>
2021-05-19 15:36:36 +0200

    Creating basic structure of project

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Form = () => {
+    return ( 
+       <form>
+           <input type="text"/>
+           <button>Wyszukaj miasta</button>
+       </form>
+     );
+}
+ 
+export default Form;
