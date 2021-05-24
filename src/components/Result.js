fb807e97a292dfa4f994a01f4fdc909e21e4bbd4
Your Name <you@example.com>
2021-05-24 17:00:41 +0200

    adding styles

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Result.css';
 
 const Result = props => {
 
@@ -11,14 +12,15 @@ const Result = props => {
         const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString();
         const sunsetTime = new Date(sunset * 1000).toLocaleTimeString();
         content = (
-            <div><h3>Wyniki wyszukiwania dla <em>{city}</em></h3>
+            <>
+            <h3>Wyniki wyszukiwania dla <em>{city}</em></h3>
             <h4>Dane dla dnia i godziny: {date}</h4>
             <h4>Aktualna temperatura: {temp} &#176;C</h4>
             <h4>Wschód słońca dzisiaj o {sunriseTime}</h4>
             <h4>Zachód słońca dzisiaj o {sunsetTime}</h4>
             <h4>Aktualna siła wiatru {wind} m/s</h4>
             <h4> Aktualne ciśnienie {pressure} hPa</h4>
-            </div>
+            </>
         )
     }
 
