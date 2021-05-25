61a90a22047528360633d0189f8bc79531ce82a9
Your Name <you@example.com>
2021-05-25 21:55:19 +0200

    compontent life as a searching just in time

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,13 @@ import React from 'react';
 
 const Form = props => {
     return ( 
-       <form onSubmit={props.submit}>
+       <form>
            <input 
            type="text"
            value={props.value}
            onChange = {props.change}
            placeholder="wpisz miasto"
            />
-           <button>Wyszukaj miasta</button>
        </form>
      );
 }
